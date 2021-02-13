b4b6d29b1f83c96cbf71389659a0c10c8d682c0d
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2021-02-13 01:07:08 -0300

    Add cart component
    This component displays the items in the cart.
    It allows changing the amount of each item.
    It shows the total price.

---
+++ src/Cart/Cart.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import CartItem from '../CartItem/CartItem';
+// Types
+import { CartItemType } from '../App';
+// Styles
+import { Wrapper } from './Cart.styles';
+
+type Props = {
+  cartItems: CartItemType[];
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((acc: number, item) => acc + item.amount * item.price, 0);
+
+  return (
+    <Wrapper>
+      <h2>Your shopping cart</h2>
+      {cartItems.length === 0 ? <p>No items in cart</p> : null}
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />
+      ))}
+      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
+    </Wrapper>
+  );
+};
+
+export default Cart;
